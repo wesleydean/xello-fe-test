fa2af918120b9fb9a4b32d56c80b0dd78bb49866
wesleydean <wesleyf@careercruising.com>
2022-11-22 09:04:48 -0500

    5 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InMemoryService } from './services/in-memory.service';
+import { LocaleService } from './services/locale.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [LocaleService, InMemoryService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
